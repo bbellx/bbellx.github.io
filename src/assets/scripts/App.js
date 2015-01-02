51dad9060888a9438ddab8a5ef58dd4d4a4c2137
Brian Bell <bbell@nerdery.com>
2015-01-02 16:38:41 -0600

    initial codebase commit.

---
+++ src/assets/scripts/App.js
@@ -0,0 +1,34 @@
+define(function(require, exports, module) { // jshint ignore:line
+    'use strict';
+    
+    var Modernizr = require('modernizr');
+    var AutoReplace = require('nerdery-auto-replace');
+    var ExternalLinks = require('nerdery-external-links');
+
+    /**
+     * Initial application setup. Runs once upon every page load.
+     *
+     * @class App
+     * @constructor
+     */
+    var App = function() {
+        AutoReplace.init();
+        ExternalLinks.init();
+        this.init();
+    };
+
+    var proto = App.prototype;
+
+    /**
+     * Initializes the application and kicks off loading of prerequisites.
+     *
+     * @method init
+     * @private
+     */
+    proto.init = function() {
+        // Create your views here
+    };
+
+    return App;
+
+});
